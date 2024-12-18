0e05271623ceccb0aaf50b8d600aa202f2156eff
Leeseein <huangha607@naver.com>
2024-12-18 21:36:24 +0900

    Feat: 토큰 저장소 추가

---
+++ service/tokenStorage.ts
@@ -0,0 +1,30 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+export const TOKEN_STORAGE_KEY = {
+  ACCESS_TOKEN: 'ACCESS_TOKEN',
+  REFRESH_TOKEN: 'REFRESH_TOKEN',
+};
+
+export const tokenStorage = {
+  getAccessToken: async () => {
+    return await AsyncStorage.getItem(TOKEN_STORAGE_KEY.ACCESS_TOKEN);
+  },
+
+  getRefreshToken: async () => {
+    return await AsyncStorage.getItem(TOKEN_STORAGE_KEY.REFRESH_TOKEN);
+  },
+
+  setTokens: async (accessToken: string, refreshToken: string) => {
+    await AsyncStorage.multiSet([
+      [TOKEN_STORAGE_KEY.ACCESS_TOKEN, accessToken],
+      [TOKEN_STORAGE_KEY.REFRESH_TOKEN, refreshToken],
+    ]);
+  },
+
+  removeTokens: async () => {
+    await AsyncStorage.multiRemove([
+      TOKEN_STORAGE_KEY.ACCESS_TOKEN,
+      TOKEN_STORAGE_KEY.REFRESH_TOKEN,
+    ]);
+  }
+};
