535a99f8d3152940b2da6e8e3ce462525ed4a980
Leeseein <huangha607@naver.com>
2024-12-20 14:12:47 +0900

    Feat: 하단 탭 네비게이션 추가

---
+++ app/(tabs)/sns/index.tsx
@@ -0,0 +1,22 @@
+import { Text, View, StyleSheet } from "react-native";
+
+export default function Profile() {
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.text}>갱쥐sns 화면</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#25292e',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    text: {
+        color: '#fff',
+    },
+});
