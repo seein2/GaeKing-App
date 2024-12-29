da76fc02617242d3ad2dcdcc4b8d5a0ad785758e
Leeseein <huangha607@naver.com>
2024-12-29 18:09:53 +0900

    Rename: 파일이름 변경

---
+++ app/(tabs)/schedules/index.tsx
@@ -0,0 +1,23 @@
+import { Text, View, StyleSheet } from "react-native";
+import { useUser } from "@/context/userContext";
+
+export default function SchedulesLayout() {
+  const { user } = useUser();
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>반갑습니다! {user?.user_name}</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#25292e',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: '#fff',
+  },
+});
