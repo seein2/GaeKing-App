8f7d54d5f80ea099046e05be79b574e82488f11b
Leeseein <huangha607@naver.com>
2025-01-03 11:23:32 +0900

    Fix: config파일

--- config/config.ts
+++ config/config.ts
@@ -1,16 +1,16 @@
 const ENV = {
-    development: {
-    //   API_URL: 'http://172.30.1.2:3000'  // 집
-      API_URL: 'http://10.100.1.12:3000' // 학원
-    },
-    production: {
-      API_URL: ''  // 운영 서버 주소
-    }
-  };
-  
-  // 현재 환경 설정
-  const getEnvVars = () => {
-    return ENV.development;  // 배포시엔 ENV.production
-  };
-  
-  export default getEnvVars();
+  development: {
+    // API_URL: 'http://175.192.217.28:3000'
+    API_URL: 'http://:3000' // 서버주소
+  },
+  production: {
+    API_URL: ''  // 운영 서버 주소
+  }
+};
+
+// 현재 환경 설정
+const getEnvVars = () => {
+  return ENV.development;  // 배포시엔 ENV.production
+};
+
+export default getEnvVars();
