8aa51242260af990036787d8e43fd95705aa1b5c
Leeseein <huangha607@naver.com>
2025-01-12 21:26:07 +0900

    Feat: 위젯 설정 service 추가

---
+++ service/widget.tsx
@@ -0,0 +1,22 @@
+import api from "./api";
+
+const widget = {
+    getSettings: async (dog_id: Number): Promise<WidgetResponse> => {
+        try {
+            const response = await api.get(`/widget/settings/${dog_id}`);
+            return response.data;
+        } catch (error) {
+            throw error;
+        }
+    },
+    updateSettings: async (dog_id: Number, data: WidgetTypes) => {
+        try {
+            const response = await api.post(`/widget/${dog_id}`, data);
+            return response.data;
+        } catch (error) {
+            throw error;
+        }
+    },
+};
+
+export default widget;
