c267a9277e62906bc33eb180c749e74f99022915
Leeseein <huangha607@naver.com>
2025-01-15 14:51:50 +0900

    Refactor: 커밋 - Changes to be committed파일 커밋

--- config/config.ts
+++ config/config.ts
@@ -1,7 +1,7 @@
 const ENV = {
   development: {
     // API_URL: 'http://175.192.217.28:3000'
-    API_URL: 'http://:3000' // 서버주소
+    API_URL: 'http://172.30.1.55:3000'
   },
   production: {
     API_URL: ''  // 운영 서버 주소
